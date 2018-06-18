12a66b2a8360ac06d3aed54e59950e7b3ff25bbc
Digital Studio ID <digitalstudio.id@gmail.com>
2018-06-18 08:33:16 +0700

    Add yield for additional reference css, js and plugin
    Add yield for additional reference css, js and plugin

--- resources/views/vendor/adminlte2/sample/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte2/sample/partials/htmlheader.blade.php
@@ -35,4 +35,6 @@
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  @yield('htmlheader_additional')
 </head>
--- resources/views/vendor/adminlte2/sample/partials/scripts.blade.php
+++ resources/views/vendor/adminlte2/sample/partials/scripts.blade.php
@@ -38,3 +38,5 @@
 <script src="{{ asset('dist/js/pages/dashboard2.js') }}"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="{{ asset('dist/js/demo.js') }}"></script>
+
+@yield('script_additional')
