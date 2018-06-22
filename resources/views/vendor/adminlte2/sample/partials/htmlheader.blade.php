7e13bf5811e399d216e5494616154b28dbecf294
Digital Studio ID <digitalstudio.id@gmail.com>
2018-06-23 06:41:49 +0700

    Change get title name method
    Change get title name method. The result get from .env

--- resources/views/vendor/adminlte2/sample/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte2/sample/partials/htmlheader.blade.php
@@ -1,7 +1,11 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 - {{ $pageTitle }}</title>
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'DSID Framework v.01') }} - {{ $pageTitle }}</title>
+
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
