6adfba3ad7016ba1ffe6665933d432a00894cf10
Digital Studio ID <digitalstudio.id@gmail.com>
2018-06-23 05:53:08 +0700

    Tidy up the scripts
    Tidy up the scripts

--- resources/views/vendor/adminlte2/layouts/partials/controlsidebar.blade.php
+++ resources/views/vendor/adminlte2/layouts/partials/controlsidebar.blade.php
@@ -186,7 +186,5 @@
     <!-- /.tab-pane -->
   </div>
 </aside>
-<!-- /.control-sidebar -->
-<!-- Add the sidebar's background. This div must be placed
-     immediately after the control sidebar -->
+
 <div class="control-sidebar-bg"></div>
