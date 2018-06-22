d31960979a19484befdf45cd9f258eb3dfbef7f4
Digital Studio ID <digitalstudio.id@gmail.com>
2018-06-23 05:51:59 +0700

    Tidy up the scripts
    Tidy up the scripts

--- resources/views/vendor/adminlte2/layouts/partials/contentheader.blade.php
+++ resources/views/vendor/adminlte2/layouts/partials/contentheader.blade.php
@@ -1,10 +1,13 @@
 <section class="content-header">
+	
 	<h1>
-		Dashboard
-		<small>Control panel</small>
+		{{ $pageTitle }}
+		<small>{{ $pageDescription }}</small>
 	</h1>
+
 	<ol class="breadcrumb">
-		<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-		<li class="active">Dashboard</li>
+		<li><a href="{{ route('home') }}"><i class="fa fa-home"></i> Home</a></li>
+		@yield('contentheader_breadcrumb')
 	</ol>
+
 </section>
