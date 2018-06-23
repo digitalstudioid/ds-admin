2e91369b7f266ae93db58cc0f7ab9328eb84d93f
Digital Studio ID <digitalstudio.id@gmail.com>
2018-06-23 12:22:45 +0700

    Tidy up the scripts
    Tidy up the scripts

--- resources/views/vendor/adminlte2/layouts/partials/mainheader.blade.php
+++ resources/views/vendor/adminlte2/layouts/partials/mainheader.blade.php
@@ -1,7 +1,7 @@
 <header class="main-header">
 
   <!-- Logo -->
-  <a href="{{ url('adminlte') }}" class="logo">
+  <a href="{{ url('home') }}" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini"><b>A</b>LT</span>
     <!-- logo for regular state and mobile devices -->
